8db6e097d0ebe5ea93fcc91989278a6bbdc085ac
johnnybro360 <abest82@gmail.com>
2023-02-28 16:19:12 +0800

    aa

--- src/cat.py
+++ src/cat.py
@@ -1,6 +1,10 @@
+from typing import Optional, List, Sequence
+from collections.abc import Sequence
+
 """Module docstring"""
 class Dog:
     name = 'fido'
+
 
 class Animal:
     def make_sound(self):
@@ -8,15 +12,26 @@ class Animal:
 
 
 class Cat(Animal):
+    species = 'Felis Catus'
     """This class defines a cat"""
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, names: Sequence[str]) -> None:
+        self.name = names
+        self.eat: Optional[bool] = None
+        self.my_species = Cat.species
+        self.species = 'Felis Catus1'+ Cat.species
 
+    @staticmethod
+    def make_generic_sound():
+        print("SOUND")
 
-    def make_sound(self):
+    @classmethod
+    def make_generic_blink(cls):
+        ...
+
+    def make_sound(self) -> str:
         super().make_sound()
         """method docstring"""
-        print(f"{self.name} Meows Prrrs Meows")
+        return f"{self.name} Meows Prrrs Meows"
 
     # def __repr__(self):
     #     return f"Cat()"
@@ -26,9 +41,12 @@ class Cat(Animal):
 
 
 if __name__ == '__main__':
+
     from pprint import pprint
-    best_cat = Cat('Lenny')
+    best_cat = Cat(123)
     best_cat.make_sound()
     doggy = Dog()
+    Cat.make_generic_sound()
     # pprint(dir(lenny))
-
+    print(Cat.species)
+    print(best_cat.species)
